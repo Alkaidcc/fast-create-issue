05f41d65f39cc6cea746ee82e35c095d8281ea45
Alkaidcc <alkaidcc@qq.com>
2024-05-10 10:11:28 +0800

    chore: init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button type="button">count is {{ count }}</button>
+  </div>
+</template>
+
+<script setup>
+// some comment
+const aaa = ref('https://www.zhihu.com')
+
+</script>
